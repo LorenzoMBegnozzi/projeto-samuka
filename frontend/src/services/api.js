ece5663ca189aa42f7c88dccc1cc23e63e9bccdf
Lorenzo Marzola Begnozzi <lorenzobegnozzi@hotmail.com>
2026-01-21 21:11:40 -0300

    rodar servidor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: `/api`, // Usando proxy definido no vite.config.js
 });
 
 // Interceptor para adicionar token automaticamente
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5173,
+    strictPort: true,
+    allowedHosts: 'all',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });
